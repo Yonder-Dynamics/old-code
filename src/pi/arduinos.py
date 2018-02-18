c7807f2143c7459de9a21b87c98ae7e4df4a6d76
Simon Fong <simonfong6@gmail.com>
2018-02-17 16:09:04 -0800

    ?

--- src/pi/arduinos.py
+++ src/pi/arduinos.py
@@ -1,6 +1,6 @@
 import smbus
 import time
-# for RPI version 1, use "bus = smbus.SMBus(0)"
+
 bus = smbus.SMBus(1)
 
 # This is the address we setup in the Arduino Program
@@ -8,7 +8,6 @@ address = 0x04
 
 def writeNumber(value):
     bus.write_byte(address, value)
-    # bus.write_byte_data(address, 0, value)
     return -1
 
 def readNumber():
@@ -17,7 +16,7 @@ def readNumber():
     return number
 
 while True:
-    var = input("Enter 1 - 9: ")
+    var = raw_input("Enter 1 - 9: ")
     if not var:
         continue
 
